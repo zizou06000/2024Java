bd5d5c5e35edce9de739dd96cd7e31ca79a9573f
Zaid <zaidhassaoui7@gmail.com>
2025-02-11 14:54:03 +0100

    depot

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Voiture voiture = new Voiture ();
+        voiture.marque ="Toyota";
+        voiture.modele = "Corolla";
+        voiture.couleur = "Rouge";
+
+        voiture.demarrer();
+
+    }
+}
---
+++ Voiture.java
@@ -0,0 +1,17 @@
+package debut_poo;
+
+public class Voiture{
+    string marque;
+    string modele;
+    string couleur;
+
+    void accelerer (){
+        System.out.println("la voiture accelere");
+    }
+    void freiner (){
+        System.out.println("la voiture freine");
+    }
+    void demarrer(){
+        System.out.println("la voiture demarre");
+    }
+}
